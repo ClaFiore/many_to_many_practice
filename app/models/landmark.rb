daacf5d338f254ead6afdb3fc8601f5605d3d396
Claudia Borghini <claudiaborghini@Claudias-MacBook-Air.local>
2020-07-22 13:03:45 +0200

    Done

--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -1,1 +1,30 @@
-# put your Landmark model here
+class Landmark
+    attr_reader :name, :city
+    @@all = []
+
+    def initialize(name, city)
+        @name = name
+        @city = city
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_city(city)
+        self.all.select do |landm|
+            landm.city == city end
+    end
+
+    def trips
+        Trip.all.select do |trip|
+            trip.landmark == self end
+    end
+
+    def tourists
+        self.trips.map do |trip|
+            trip.tourist end
+    end
+
+end
--- app/models/tourist.rb
+++ app/models/tourist.rb
@@ -1,1 +1,39 @@
-# put your Tourist model here
+class Tourist
+    attr_reader :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_name(name)
+        self.all.find do |tourist|
+            tourist.name == name
+        end
+    end
+
+    def trips
+        Trip.all.select do |tour_landm|
+            tour_landm.tourist == self end
+    end
+
+    def landmarks
+        self.trips.map do |trip|
+            trip.landmark 
+        end
+    end
+
+    def visit_landmark(landmark)
+        Trip.new(self, landmark)
+    end
+
+    def never_visited
+        Landmark.all - self.landmarks
+    end
+
+end
